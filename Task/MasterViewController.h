694be5a0372760cd0ddaf51b2926b588bb316f77
Naveen <its2hot@outlook.com>
2016-12-26 23:43:43 +0530

    Initial Commit

---
+++ Task/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Task
+//
+//  Created by Naveen Dangeti on 26/12/16.
+//  Copyright © 2016 Naveen Dangeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ Task/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Task
+//
+//  Created by Naveen Dangeti on 26/12/16.
+//  Copyright © 2016 Naveen Dangeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
